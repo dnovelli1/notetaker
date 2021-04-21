60be4cba28414ba1b8be102f49cab8f4c7bfdea5
Jake Novelli <jakenovelli11@gmail.com>
2021-04-20 18:23:28 -0700

    added server

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT||3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.use('/api/notes', require('./routes/apiroutes'));
+app.use('/', require('./routes/htmlroutes'));
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`App listening on PORT ${PORT}`);
+  });
